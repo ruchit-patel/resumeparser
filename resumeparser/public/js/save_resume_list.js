ec3f574b96f0aa1f37e18faba22fe4110c6e2b1e
sayaji-jay <jaypatelsayaji@gmail.com>
2025-04-28 05:12:28 +0000

    Add filter for current user in saveresunes

--- resumeparser/public/js/save_resume_list.js
+++ resumeparser/public/js/save_resume_list.js
@@ -1,16 +1,9 @@
 console.log("---------------Save Resume list scripts loaded---------------");
 
+
 frappe.listview_settings['Save Resume'] = {
     onload: function (listview) {
-        // Add a button to the top of the list view
-        listview.page.add_inner_button(__('View Selected Resume'), () => {
-            const selected = listview.get_checked_items();
-            if (selected.length === 1) {
-                window.open(`/resume_search/detail/${selected[0].resume}`, '_blank');
-            } else {
-                frappe.msgprint(__('Please select a single resume to view'));
-            }
-        });
+        listview.filter_area.add([[listview.doctype, 'user', '=', frappe.session.user]]);
     },
     button: {
         show: function(doc) {
